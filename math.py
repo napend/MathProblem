b3aa2fb3b55329d1c59969a4bea5f7c850681085
alexmunoz905 <alex.munoz905@gmail.com>
2016-01-28 18:15:13 -0500

    Uploading python files

---
+++ math.py
@@ -0,0 +1,18 @@
+#This app does your math
+
+print("Please enter your math problem, Addition Only!")
+print("  ")
+
+print("Enter your first number:")
+first = input("")
+
+print("  ")
+print("Enter your 2nd number")
+second = input("")
+
+answer = first + second
+
+print("  ")
+print(answer)
+
+print("  ")
---
+++ math2.py
@@ -0,0 +1,16 @@
+#This app does your math
+
+print("If you want addition say yes, if you dont say no.")
+
+addition = input("")
+
+print("  ")
+print("Enter your 2nd number")
+second = input("")
+
+answer = first + second
+
+print("  ")
+print(answer)
+
+print("  ")
